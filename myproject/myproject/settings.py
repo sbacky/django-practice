c1d1031f5fb860d02ee04677f33afaf03c8429d1
Neil <nhc.crum@outlook.com>
2022-09-04 15:55:44 -0700

    Added mypy typing and Django-Stubs typing extension. FIxed nitty in settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-cee9@_kc**gf+^+0y)^v15gmkr=*h$5p7+nsy$ndf3m=!&6d%i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
