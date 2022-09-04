2a6041fea85152b354be2a7bd297d4a20d102c54
Neil <nhc.crum@outlook.com>
2022-09-04 08:35:16 -0700

    Create signal between User and Profile models in model.py

--- myproject/social/models.py
+++ myproject/social/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -12,3 +14,15 @@ class Profile(models.Model):
     
     def __str__(self) -> str:
         return self.user.username
+
+
+# Use decorator to Create a Profile for each new user.
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # Can use set() instead of add() when need to add multiple references to an object
+        # user_profile.follows.set([instance.profile.id])
+        user_profile.follows.add([instance.profile])
+        user_profile.save()
