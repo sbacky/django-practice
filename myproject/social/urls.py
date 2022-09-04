733b9520dbdd8ecba7e89990700d361ce9d58f14
Neil <nhc.crum@outlook.com>
2022-09-04 09:36:22 -0700

    Add profile_list.html to templates/social/, a profile_list view function to display template and /profile_list/ url to urls.py

--- myproject/social/urls.py
+++ myproject/social/urls.py
@@ -1,10 +1,11 @@
 # social/urls.py
 
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, profile_list
 
 app_name = "social"
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("profile_list/", profile_list, name="profile_list"),
 ]
--- myproject/social/views.py
+++ myproject/social/views.py
@@ -1,6 +1,11 @@
 # social/views.py
 
 from django.shortcuts import render
+from .models import Profile
 
 def dashboard(request):
     return render(request, "base.html")
+
+def profile_list(request):
+    profiles = Profile.objects.exclude(user=request.user)
+    return render(request, "social/profile_list.html", {"profiles": profiles})
