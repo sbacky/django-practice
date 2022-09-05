9bbdd397cc09bc84e2ca97a40571c6bda974e98a
Neil <nhc.crum@outlook.com>
2022-09-04 19:16:25 -0700

    Add typing to models.py

--- myproject/social/models.py
+++ myproject/social/models.py
@@ -1,11 +1,12 @@
+from typing import ClassVar, Union
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    follows = models.ManyToManyField(
+    user: ClassVar = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows: ClassVar = models.ManyToManyField(
         "self",
         related_name="followed_by",
         symmetrical=False,
@@ -24,5 +25,5 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.save()
         # Can use set() instead of add() when need to add multiple references to an object
         # user_profile.follows.set([instance.profile.id])
-        user_profile.follows.add([instance.profile])
+        user_profile.follows.add(instance.profile)
         user_profile.save()
