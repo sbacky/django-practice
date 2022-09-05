3a235196fd55e634e9563de56f8517805050d943
Neil <nhc.crum@outlook.com>
2022-09-04 19:16:43 -0700

    Add logic to handle POST request logic for follow/unfollow buttons on profile.html

--- myproject/social/views.py
+++ myproject/social/views.py
@@ -1,6 +1,7 @@
 # social/views.py
 
 from django.shortcuts import render
+
 from .models import Profile
 
 def dashboard(request):
@@ -12,4 +13,13 @@ def profile_list(request):
 
 def profile(request, pk):
     profile = Profile.objects.get(pk=pk)
+    if request.method =="POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == 'unfollow':
+            current_user_profile.follows.remove(profile)
+        current_user_profile.save()     
     return render(request, "social/profile.html", {"profile": profile})
